69aa12e1d25acce9e74750d93477033168b35919
Padma <padmalathatce@gmail.com>
2024-08-08 16:34:43 -0700

    Add image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { appleImg } from "../utils";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header>
+      <nav>
+        <img
+          src={appleImg}
+          alt="apple"
+          className="src"
+          width={14}
+          height={18}
+        />
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
