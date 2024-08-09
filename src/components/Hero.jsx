cc8154ad66a732ea92d206a7959d171dd474206c
Padma <padmalathatce@gmail.com>
2024-08-08 17:24:41 -0700

    Hero Page Update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="w-full nav-height bg-black relaive">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p className="hero-title">Happy Birthday Vignesh</p>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
