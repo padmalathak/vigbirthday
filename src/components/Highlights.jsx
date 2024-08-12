b5c9c0159458c648684f010a39af1e7cbd833200
Padma <padmalathatce@gmail.com>
2024-08-11 20:49:07 -0700

    Highlights

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,22 +1,39 @@
 import { useGSAP } from "@gsap/react";
 import React from "react";
 import gsap from "gsap";
+import { rightImg, watchImg } from "../utils";
 
 function Highlights() {
   useGSAP(() => {
     gsap.to("#title", {
       opacity: 1,
       y: 0,
-      delay: 2,
+    });
+
+    gsap.to(".link", {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.25,
     });
   });
   return (
     <section id="highlights" className="bg-zinc h-full w-screen common-padding">
       <div className="screen-max-width">
-        <div className="mb-12 w-full item-end justfiy-between">
+        <div className="mb-4 w-full md:flex flex-end justify-between">
           <h1 id="title" className="section-heading">
             Celebrating Vignesh
           </h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch Cute Vignesh
+              <img src={watchImg} alt="watch" className="ml-2"></img>
+            </p>
+            <p className="link">
+              Watch Leelaigal
+              <img src={rightImg} alt="right" className="ml-2"></img>
+            </p>
+          </div>
         </div>
       </div>
       Highlights
