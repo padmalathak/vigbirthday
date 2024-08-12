cbf53d04c107d0a54ff1c19ace596fba2dd46a95
Padma <padmalathatce@gmail.com>
2024-08-11 21:07:52 -0700

    video carousel

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { hightlightsSlides } from "../constants";
 
 function VideoCarousel() {
-  return <div>VideoCarousel</div>;
+  return (
+    <div className="flex items-center">
+      {hightlightsSlides.map((slides, i) => {
+        return (
+          <div key={slides.id} id="slider" className="sm:pr-20 pr-10">
+            <div className="video-carousel_container">
+              <div className="w-full h-full bg-black overflow-hidden flex-center rounded-3xl"></div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default VideoCarousel;
