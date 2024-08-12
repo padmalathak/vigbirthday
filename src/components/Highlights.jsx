125934839bbcac6e37ec884ac9b30cbb18115bed
Padma <padmalathatce@gmail.com>
2024-08-11 20:33:37 -0700

    Highlighs

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,12 +1,21 @@
+import { useGSAP } from "@gsap/react";
 import React from "react";
+import gsap from "gsap";
 
 function Highlights() {
+  useGSAP(() => {
+    gsap.to("#title", {
+      opacity: 1,
+      y: 0,
+      delay: 2,
+    });
+  });
   return (
     <section id="highlights" className="bg-zinc h-full w-screen common-padding">
       <div className="screen-max-width">
-        <div>
+        <div className="mb-12 w-full item-end justfiy-between">
           <h1 id="title" className="section-heading">
-            Celebrating Vignesh Today
+            Celebrating Vignesh
           </h1>
         </div>
       </div>
