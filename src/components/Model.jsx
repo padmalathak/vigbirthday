5f71c75388c88b072248f0acdaa8131b0ec1b34c
Padma <padmalathatce@gmail.com>
2024-08-15 14:21:31 -0700

    Setting up the Canvas

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,7 +1,86 @@
-import React from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React, { useRef, useState } from "react";
+import ModelView from "./ModelView";
+import { yellowImg } from "../utils";
+
+import * as THREE from "three";
+import { View } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
 function Model() {
-  return <div>Model</div>;
+  const [size, setsize] = useState("small");
+  const [model, setmodel] = useState({
+    title: "Vignesh Habitats",
+    color: ["#8F8A81", "#FFE7B9"],
+    img: yellowImg,
+  });
+
+  //camera control for the model view
+  const cameraContolSmall = useRef();
+  const cameraContolLarge = useRef();
+
+  //model
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
+
+  //rotation
+  const [smallRotation, setsmallRotation] = useState(0);
+  const [largeRotation, setlargeRotation] = useState(0);
+
+  //title animation
+  useGSAP(() => {
+    gsap.to("#heading", {
+      y: 0,
+      opacity: 1,
+    });
+  });
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
+          My Handsome Birthday Boy
+        </h1>
+        <div className="flex flex-col items-center mt-5">
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+            <ModelView
+              index={1}
+              groupRef={small}
+              controlRef={cameraContolSmall}
+              gsapType="view1"
+              setRotationState={setsmallRotation}
+              item={model}
+              size={size}
+            />
+            <ModelView
+              index={2}
+              groupRef={large}
+              controlRef={cameraContolLarge}
+              gsapType="view1"
+              setRotationState={setlargeRotation}
+              item={model}
+              size={size}
+            />
+
+            <Canvas
+              className="w-full h-full"
+              style={{
+                position: "fixed",
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                overflow: "hidden",
+              }}
+              eventSource={document.getElementById("root")}
+            >
+              <View.Port />
+            </Canvas>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Model;
