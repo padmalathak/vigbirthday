b28de99e6e6be114d3ee04981144e20d507144fb
Padma <padmalathatce@gmail.com>
2024-08-15 14:54:44 -0700

    Model 3d

--- src/constants/index.js
+++ src/constants/index.js
@@ -45,25 +45,25 @@ export const hightlightsSlides = [
 export const models = [
   {
     id: 1,
-    title: "iPhone 15 Pro in Natural Titanium",
+    title: "Vella Manasu",
     color: ["#8F8A81", "#ffe7b9", "#6f6c64"],
     img: yellowImg,
   },
   {
     id: 2,
-    title: "iPhone 15 Pro in Blue Titanium",
+    title: "Azhagu Kannu",
     color: ["#53596E", "#6395ff", "#21242e"],
     img: blueImg,
   },
   {
     id: 3,
-    title: "iPhone 15 Pro in White Titanium",
+    title: "Kutty thoppai",
     color: ["#C9C8C2", "#ffffff", "#C9C8C2"],
     img: whiteImg,
   },
   {
     id: 4,
-    title: "iPhone 15 Pro in Black Titanium",
+    title: "Periya Biceps",
     color: ["#454749", "#3b3b3b", "#181819"],
     img: blackImg,
   },
